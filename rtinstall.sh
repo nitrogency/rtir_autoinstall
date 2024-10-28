ba5518a75c88ac83840edcc2680336514cc8a52a
Benas <129758495+nitrogency@users.noreply.github.com>
2024-10-28 13:21:04 +0200

    Made unattended

--- rtinstall.sh
+++ rtinstall.sh
@@ -1,60 +1,89 @@
+#!/bin/bash
+
+PORT=8080
+
 setup_dependencies() {
     echo "Updating system packages & installing dependencies"
-    sudo apt update -y
-    sudo apt upgrade -y
+    sudo apt update -y && sudo apt upgrade -y
+    sudo apt install -y perl make git build-essential libssl-dev libexpat1-dev \
+        libmysqlclient-dev libcrypt-ssleay-perl liblwp-protocol-https-perl \
+        mariadb-server mariadb-client nginx
+    
 
-    sudo apt install perl -y
-    sudo apt install make -y
-
-    sudo apt install git -y
-
-    sudo apt install build-essential libssl-dev libexpat1-dev libmysqlclient-dev libcrypt-ssleay-perl liblwp-protocol-https-perl -y
-
-    sudo apt install mariadb-server mariadb-client -y
-    sudo systemctl start mariadb.service
-
-    sudo apt install nginx -y
-    sudo systemctl start nginx.service
+    sudo systemctl enable --now mariadb nginx
 }
 
 rt_install() {
     echo "Installing RT"
     curl -s https://api.github.com/repos/bestpractical/rt/releases/latest | grep ".tar.gz" | cut -d '"' -f 4 | wget -qi - -O rt.tar.gz
-    echo "Downloaded"
     mkdir rt && tar xf rt.tar.gz -C rt --strip-components 1
     rm rt.tar.gz
     cd rt
 
     ./configure
     make testdeps
-    sudo cpan install -fi HTML::FormatText HTML::TreeBuilder HTML::FormatText::WithLinks HTML::FormatText::WithLinks::AndTables DBD::mysql LWP::Protocol::https Parallel::ForkManager Parallel:Prefork CPAN::DistnameInfo App::FatPacker 
-    sudo cpan install -fi Module::Pluggable Convert::Color
-    PERL_MM_OPT="skip-test" PERL_MM_USE_DEFAULT=1 | sudo make fixdeps
+    yes | sudo cpan install App::cpanminus
+    sudo cpanm --notest HTML::FormatText HTML::TreeBuilder HTML::FormatText::WithLinks \
+        HTML::FormatText::WithLinks::AndTables DBD::mysql LWP::Protocol::https \
+        Parallel::ForkManager Parallel::Prefork CPAN::DistnameInfo App::FatPacker \
+        Module::Pluggable Convert::Color Apache::Session Business::Hours CGI::Emulate::PSGI \
+        CGI::PSGI CSS::Minifier::XS CSS::Squish Class::Accessor::Fast Crypt::Eksblowfish \
+        DBIx::SearchBuilder Data::GUID Data::ICal Data::Page Date::Extract Date::Manip DateTime \
+        DateTime::Format::Natural DateTime::Locale Devel::GlobalDestruction Devel::StackTrace \
+        Email::Address Email::Address::List Encode::Detect::Detector Encode::HanExtra \
+        File::ShareDir HTML::FormatExternal HTML::Gumbo HTML::Mason HTML::Mason::PSGIHandler \
+        HTML::Quoted HTML::RewriteAttributes HTML::Scrubber IPC::Run3 JSON \
+        JavaScript::Minifier::XS List::MoreUtils Locale::Maketext::Fuzzy Locale::Maketext::Lexicon \
+        Log::Dispatch MIME::Entity MIME::Types Module::Path Module::Refresh Module::Runtime \
+        Module::Versions::Report Moose MooseX::NonMoose MooseX::Role::Parameterized Mozilla::CA \
+        Net::CIDR Net::IP Path::Dispatcher Plack Plack::Handler::Starlet Regexp::Common \
+        Regexp::Common::net::CIDR Regexp::IPv6 Role::Basic Scope::Upper Sub::Exporter \
+        Symbol::Global::Name Text::Password::Pronounceable Text::Quoted Text::Template \
+        Text::WikiFormat Text::WordDiff Text::Wrapper Time::ParseDate Tree::Simple Web::Machine \
+        XML::RSS namespace::autoclean File::Which GnuPG::Interface PerlIO::eol Crypt::X509 \
+        String::ShellQuote || exit 1
     make testdeps
     sudo make install
 
-    echo | sudo make initialize-database
-    sudo /opt/rt5/sbin/rt-server
-    echo "Installation successful. RT is running."
+    echo | sudo make initialize-database || exit 1
+    echo "Installation successful."
 }
 
 rtir_install() {
-    cd ..
     echo "Installing RTIR"
     curl -s https://api.github.com/repos/bestpractical/rtir/releases/latest | grep ".tar.gz" | cut -d '"' -f 4 | wget -qi - -O rtir.tar.gz
     mkdir rtir && tar xf rtir.tar.gz -C rtir --strip-components 1
     rm rtir.tar.gz
     cd rtir
 
-    y | sudo perl "Makefile.PL"
+    y | perl "Makefile.PL"
     sudo make install
-    echo "Plugin('RT::IR');" | sudo tee -a /opt/rt5/etc/RT_SiteConfig.pm
-    echo | sudo make initdb
+    echo "Plugin('RT::IR');" | tee -a /opt/rt5/etc/RT_SiteConfig.pm
+    echo | make initdb
     sudo systemctl restart nginx
-    sudo /opt/rt5/sbin/rt-server
-    echo "Installation successful. RTIR is running."
+    echo "Installation successful."
 }
 
-setup_dependencies
-rt_install
-rtir_install
+start() {
+
+    sudo tee /etc/systemd/system/rt-server.service > /dev/null <<EOL
+[Unit]
+Description=RT Server
+After=network.target
+
+[Service]
+Type=simple
+User=www-data  
+ExecStart=/opt/rt5/sbin/rt-server --port $PORT
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+EOL
+
+    sudo systemctl enable rt-server
+    sudo systemctl start rt-server
+
+}
+
+setup_dependencies && rt_install && rtir_install && start
